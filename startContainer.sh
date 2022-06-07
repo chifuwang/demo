ab6e943684903ffe107cdfb00bdb8129faea7a3a
Chi-Fu Wang <chifuwang@yahoo.com>
2022-06-07 05:14:41 -0500

    fix script

--- startContainer.sh
+++ startContainer.sh
@@ -27,6 +27,9 @@ echo "star container sh"
 echo "port: $1"
 echo "imageName: $2"
 echo "imageVersion: $3"
+port=$1
+imageName="$2"
+imageVersion="$3"
 echo "retrieve container id"
-containerId = $(docker ps -a -q --filter name=$1)
-echo "containerId: $containerId"
+containerId = `docker ps -a -q --filter name="${imageName}" `
+echo "containerId: ${containerId}"
